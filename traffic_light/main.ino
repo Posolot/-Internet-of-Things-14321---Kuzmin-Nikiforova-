34f8c7afacf929cdf34c5b01a57763e37dd35eee
Posolot <138215725+Posolot@users.noreply.github.com>
2024-09-30 13:21:46 +0800

    add new functio pause(work simple as delay),change manual mode work, delete options blinking yellow in manual mode and changes timings

--- traffic_light/main.ino
+++ traffic_light/main.ino
@@ -11,9 +11,20 @@
 
 bool isAutoMode = true;
 int autoState = 0;
-
 char manualCommand = 0;  
-int i;
+unsigned long previousMillis = 0;
+bool isPaused = false;
+unsigned long manualModeStartTime = 0; // время начала ручного режима
+bool manualModeTimeout = false;
+
+void pause(unsigned long duration) {
+  previousMillis = millis();
+  isPaused = true;
+  while (millis() - previousMillis < duration) {
+  }
+  isPaused = false;
+}
+
 void update_mode() {
   if (Serial.available() > 0) {
     char cmd = Serial.read();
@@ -23,10 +34,13 @@ void update_mode() {
       digitalWrite(LED_PIN_YELLOW, LOW);
       digitalWrite(LED_PIN_RED, LOW); 
       Serial.println("Manual mode activated");
+      manualModeStartTime = millis();
+      manualModeTimeout = false;
     } else if (cmd == MODE_AUTO) {
       isAutoMode = true;
       Serial.println("Automatic mode activated");
     }
+
     if (!isAutoMode) {
       manualCommand = cmd;
     }
@@ -38,48 +52,57 @@ void process_auto_mode() {
     digitalWrite(LED_PIN_GREEN, HIGH);
     digitalWrite(LED_PIN_YELLOW, LOW);
     digitalWrite(LED_PIN_RED, LOW);
-    delay(2400);
+    pause(10000);
     for (int i = 0; i < 4; i++) {
       digitalWrite(LED_PIN_GREEN, HIGH);
-      delay(300);
+      pause(250);
       digitalWrite(LED_PIN_GREEN, LOW);
-      delay(300);
+      pause(250);
     }
     autoState = 1;
   } else if (autoState == 1) {
     digitalWrite(LED_PIN_GREEN, LOW);
-    for (int i = 0; i < 4; i++) {
-      digitalWrite(LED_PIN_YELLOW, HIGH);
-      delay(400);
-      digitalWrite(LED_PIN_YELLOW, LOW);
-      delay(400);
-    }
+    digitalWrite(LED_PIN_YELLOW, HIGH);
+    digitalWrite(LED_PIN_RED, LOW);
+    pause(1000);
     autoState = 2;
   } else if (autoState == 2) {
-    digitalWrite(LED_PIN_YELLOW, LOW);  
+    digitalWrite(LED_PIN_YELLOW, LOW); 
     digitalWrite(LED_PIN_RED, HIGH);
-    delay(3000);
+    pause(7000);
+    autoState = 3;
+  }
+  else if(autoState==3){
+    digitalWrite(LED_PIN_GREEN, LOW);
+    digitalWrite(LED_PIN_YELLOW, HIGH);
+    digitalWrite(LED_PIN_RED, LOW);
+    pause(1000);
     autoState = 0;
   }
 }
 
 void process_manual_mode() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - manualModeStartTime >= 60000) {
+    Serial.println("Manual mode timeout. Returning to automatic mode.");
+    isAutoMode = true;
+    return;
+  }
+
   if (manualCommand == SET_GREEN_ON) {
-    digitalWrite(LED_PIN_GREEN, HIGH);
-    digitalWrite(LED_PIN_YELLOW, LOW);
-    digitalWrite(LED_PIN_RED, LOW);
-  } else if (manualCommand == SET_YELLOW_BLINK) {
-    digitalWrite(LED_PIN_GREEN, LOW);
-    digitalWrite(LED_PIN_RED, LOW); 
-    for (i = 0; i < 4; i++) {
+    if (digitalRead(LED_PIN_RED) == HIGH) {
+      digitalWrite(LED_PIN_RED, LOW);
       digitalWrite(LED_PIN_YELLOW, HIGH);
-      delay(250);
-      digitalWrite(LED_PIN_YELLOW, LOW);
-      delay(250);
+      pause(3000);
     }
-    digitalWrite(LED_PIN_RED, LOW);
+    digitalWrite(LED_PIN_YELLOW, LOW);
+    digitalWrite(LED_PIN_GREEN, HIGH);
   } else if (manualCommand == SET_RED_ON) {
-    digitalWrite(LED_PIN_GREEN, LOW);
+    if (digitalRead(LED_PIN_GREEN) == HIGH) {
+      digitalWrite(LED_PIN_GREEN, LOW);
+      digitalWrite(LED_PIN_YELLOW, HIGH);
+      pause(3000);
+    }
     digitalWrite(LED_PIN_YELLOW, LOW);
     digitalWrite(LED_PIN_RED, HIGH);
   }
