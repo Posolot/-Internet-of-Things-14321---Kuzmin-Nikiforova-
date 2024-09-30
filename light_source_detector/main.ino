739349a2209167ecf5de8a8c382a8a5092f309ed
Darya778 <112837727+Darya778@users.noreply.github.com>
2024-09-30 13:11:09 +0800

    Update main.ino (updated LED algorithm depending on lighting)

--- light_source_detector/main.ino
+++ light_source_detector/main.ino
@@ -3,6 +3,12 @@
 #define PIN_LED_2 10
 #define PIN_PHOTO_SENSOR_2 A1
 
+const int sensitivity = 100;
+bool isBlinking = false;
+
+bool led1State = LOW;
+bool led2State = LOW;
+
 unsigned long previousMillis = 0;
 bool isPaused = false;
 void pause(unsigned long duration) {
@@ -23,24 +29,24 @@ void loop() {
   int val_1 = analogRead(PIN_PHOTO_SENSOR_1);
   int val_2 = analogRead(PIN_PHOTO_SENSOR_2);
   
-  if (val_1 - val_2 >= 100) {
-    digitalWrite(PIN_LED_1, LOW);
-    while(val_1 - val_2 >= 100){
-      digitalWrite(PIN_LED_2, HIGH);
-      pause(1000);
-      digitalWrite(PIN_LED_2, LOW);
-      pause(1000);
-    }
-  } else if (val_2 - val_1 >= 100) {
+  if (val_2 - val_1 >= sensitivity) {
+    digitalWrite(PIN_LED_1, led1State);
+    led1State = !led1State;
     digitalWrite(PIN_LED_2, LOW);
-    while(val_2 - val_1 >= 100){
-      digitalWrite(PIN_LED_2, HIGH);
-      pause(1000);
-      digitalWrite(PIN_LED_2, LOW);
-      pause(1000);
-    }
+    isBlinking = true;
+  } else if (val_1 - val_2 >= sensitivity) {
+    digitalWrite(PIN_LED_2, led2State);
+    led2State = !led2State;
+    digitalWrite(PIN_LED_1, LOW);
+    isBlinking = true;
   } else {
-    digitalWrite(PIN_LED_2, LOW);
-    digitalWrite(PIN_LED_1, LOW);
-  } 
+    isBlinking = false;
+    digitalWrite(PIN_LED_1, HIGH);
+    digitalWrite(PIN_LED_2, HIGH);
+  }
+
+  if (isBlinking) {
+    pause(1000);
+  }
+
 }
