d4f5b0a917b76d51c54724c920b6c506f34c63bc
Darya778 <112837727+Darya778@users.noreply.github.com>
2024-10-29 22:52:10 +0800

    Create Config.h (for photo)

---
+++ LedPhotoMQTT/photo_mqtt/Config.h
@@ -0,0 +1,10 @@
+String ssidAP = "ESP_WIFI_lemon"; // имя контроллера и точки доступа
+String passwordAP = "123412341234"; // пароль точки доступа
+
+const char* ssidCLI = "";
+const char* passwordCLI = "";
+
+const char* mqtt_broker = "broker.emqx.io";
+const int mqtt_port = 1883;
+
+const int led = 2;
