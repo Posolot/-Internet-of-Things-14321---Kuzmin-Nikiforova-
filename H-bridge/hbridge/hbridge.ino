e6f279e1068f1b700eb7316ff06118d4fe90d19e
Posolot <138215725+Posolot@users.noreply.github.com>
2025-05-28 12:34:05 +0800

    Update hbridge.ino

--- H-bridge/hbridge/hbridge.ino
+++ H-bridge/hbridge/hbridge.ino
@@ -1,68 +1,36 @@
-#include <Servo.h>
+#define FORWARD_PIN 5
+#define BACKWARD_PIN 6
+#define PWM_PIN 9
 
-Servo myservo;
-int pos = 0;
-
-void moveServo(int speedValue, bool direction) {
-  int step = map(speedValue, 0, 100, 1, 5);
-
-  if (direction) {
-    for (pos = 0; pos <= 180; pos += step) {
-      myservo.write(pos);
-      delay(map(speedValue, 0, 100, 50, 5));
-    }
-  } else {
-    for (pos = 180; pos >= 0; pos -= step) {
-      myservo.write(pos);
-      delay(map(speedValue, 0, 100, 50, 5));
-    }
-  }
-}
-
-void autoMode() {
-  int speedValue = 100;
-  for (int i = 0; i < 5; i++) {
-    moveServo(speedValue, true);
-    moveServo(speedValue, false);
-    speedValue -= 20;
-    if (speedValue < 0) speedValue = 0;
-  }
-}
-
-void manualMode() {
-  Serial.println("Введите скорость (0-100) и направление (0 - назад, 1 - вперед):");
-  while (true) {
-    if (Serial.available()) {
-      int speedValue, direction;
-      if (sscanf(Serial.readStringUntil('\n').c_str(), "%d %d", &speedValue, &direction) == 2) {
-        if (speedValue >= 0 && speedValue <= 100 && (direction == 0 || direction == 1)) {
-          moveServo(speedValue, direction);
-        } else {
-          Serial.println("Ошибка: Введите корректные значения.");
-        }
-      } else {
-        Serial.println("Ошибка ввода. Повторите попытку.");
-      }
-      Serial.println("Введите новую команду или перезапустите для выхода из ручного режима.");
-    }
-  }
-}
-
+int speed = 200;
+bool direction = true;
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);
-  Serial.println("Выберите режим: 1 - Авто, 2 - Ручной ввод");
+
+  pinMode(FORWARD_PIN, OUTPUT);
+  pinMode(BACKWARD_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  Serial.println("You can input speed and direction like this:");
+  Serial.println("100 0");
+  Serial.println("where 100 is speed, 0 or 1 is derection (1 forward, 0 backward)");
 }
 
 void loop() {
-  while (!Serial.available());
-  int mode = Serial.readStringUntil('\n').toInt();
-  
-  if (mode == 1) {
-    autoMode();
-  } else if (mode == 2) {
-    manualMode();
+  if (Serial.available() > 0) {
+    speed = Serial.parseInt();
+    direction = Serial.parseInt();
+  }
+  move();
+}
+
+void move() {
+  if (direction) {
+    digitalWrite(FORWARD_PIN, HIGH);
+    digitalWrite(BACKWARD_PIN, LOW);
+    analogWrite(PWM_PIN, speed);
   } else {
-    Serial.println("Неверный режим. Повторите ввод.");
+    digitalWrite(FORWARD_PIN, LOW);
+    digitalWrite(BACKWARD_PIN, HIGH);
+    analogWrite(PWM_PIN, speed);
   }
 }
